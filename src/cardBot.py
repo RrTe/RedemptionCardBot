81b4b292b7b2f0f31109deb75ff66b3b68955f27
Reth <32963007+RrTe@users.noreply.github.com>
2024-03-21 23:08:32 +0100

    Update cardBot.py

--- src/cardBot.py
+++ src/cardBot.py
@@ -3,10 +3,13 @@
 
 import json
 import os
-import discord
-from discord import app_commands
-from discord.ext import commands
 import re
+
+import discord
+from discord import app_commands, embeds
+from discord.ext import commands
+from typing import Dict, Any
+
 #from dotenv import load_dotenv, dotenv_values
 
 PATTERN = r'\[\[([^\]]+)\]\]'
@@ -50,26 +53,26 @@ async def test(ctx):
 """
 
 # create 25 entries for auto completion of card names based on the user input
-async def cardEntries(
+async def cardNameEntries(
     interaction: discord.Interaction,
     current: str,
 ) -> list[app_commands.Choice[str]]:
   #cards = ['Card1', 'Card2', 'Card3', 'Card4']
-  cardList = []
+  cardNameList = []
   #print(f'Cards: {len(cards)}')
   if current == '':  # nothing typed in by the user yet => return all cards, but only first 25 options
     for card in cards[:24]:
-      cardList.append(
+      cardNameList.append(
           app_commands.Choice(name=card["Name"] + ' (' + card["Set"] + ')', value=card["ImageFile"]))
   else:
     for card in cards:
       if current.lower() in card["Name"].lower():
         #print(f'Current: {current.lower()} - card name: {card["Name"].lower()}')
-        cardList.append(
+        cardNameList.append(
             app_commands.Choice(name=card["Name"] + ' (' + card["Set"] + ')', value=card["ImageFile"]))
   #print(f'Cards filtered: {len(cardList)}')
   #print(cardList[:24])
-  return cardList[:24]  # only 25 options supported
+  return cardNameList[:24]  # only 25 options supported
 """
   return [app_commands.Choice(name=card['Name'] + ' (' + card["Set"] + ')', value=card['ImageFile']) for card in cards if current.lower() in card['Name'].lower()]
 """
@@ -77,13 +80,48 @@ async def cardEntries(
 # bot command for listing card names and resolve chosen card name into card-image-url
 @bot.tree.command(name="card", description="Show a card")
 @app_commands.describe(cards="Cards to choose from")
-@app_commands.autocomplete(cards=cardEntries)
+@app_commands.autocomplete(cards=cardNameEntries)
 async def card(interaction: discord.Interaction, cards: str):
   #print(f'http://www.redemptionquick.com/lackey/sets/setimages/general/{cards}.jpg')
   await interaction.response.send_message(
       f'http://www.redemptionquick.com/lackey/sets/setimages/general/{cards}.jpg'
   )
 
+# extract all cards from carddata.json which matches the given card name pattern
+# only first card found per match will be taken
+async def extractCards(cardNames: list[str]) -> list[Dict[str, Any]]:
+  cardList = []
+  for cardName in cardNames:
+    for card in cards:
+      if cardName.lower() in card["Name"].lower(): # first card found
+        cardList.append(card)
+        break # only select first card
+  return cardList
+
+async def createEmbeds(cardList: list[Dict[str, Any]]) -> list[discord.Embed]:
+  embedList = []
+  for card in cardList:
+    cardImageURL = "http://www.redemptionquick.com/lackey/sets/setimages/general/" + \
+                    card["ImageFile"] + ".jpg"
+    embed=discord.Embed(title = card["Name"], url = cardImageURL, description = card["Type"])
+    embed.set_thumbnail(url = card_image_url)
+    embed.add_field(name = "card_set", value = card["Set"], inline=True)
+    embed.add_field(name = "card_identifiers", value = card["Identifier"], inline=True)
+    embed.add_field(name = "card_ability", value = card["SpecialAbility"], inline=True)
+    embed.add_field(name = "card_stats", value = card["Strength"] + "/" + card["Toughness"],
+                    inline=True)
+    embed.add_field(name = "card_alignment", value = card["Alignment"], inline=True)
+    embed.add_field(name = "card_brigades", value = card["Brigade"], inline=True)
+    embed.add_field(name = "card_class", value = card["Class"], inline=True)
+    embed.add_field(name = "card_reference", value = card["Reference"], inline=True)
+    embed.add_field(name = "card_testament", value = card["Testament"], inline=True)
+    embed.add_field(name = "card_legality", value = card["Legality"], inline=True)
+    embed.add_field(name = "card_rarity", value = card["Rarity"], inline=True)
+    embed.set_footer(text = card["Rarity"])
+    embedList.append(embed)
+    
+  return embedList
+    
 
 @bot.event
 # Normal Bot message handling for testing
@@ -91,12 +129,18 @@ async def on_message(message):
   if message.author == bot.user:
     return
     
-  pattern_result = re.findall(PATTERN, message.content)
-  print(pattern_result)
-  print(message.content)
-
-  if pattern_result:
-    print(f'Message contains card pattern')
+  # find all entered card name patterns
+  # and for each pattern extract the first matching card
+  pattern_results = re.findall(PATTERN, message.content)
+  if pattern_results:
+    firstCardsFound = await extractCards(pattern_results)
+    if firstCardsFound:
+      await message.channel.send(embeds = createEmbeds(firstCardsFound))
+  #for result in pattern_results:  
+  #print(pattern_results)
+  #print(message.content)
+  #if pattern_results:
+    #print(f'Message contains card pattern')
 
 
 try:
